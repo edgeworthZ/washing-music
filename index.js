c1d257207581dd4aaf45f2296c9e1c5aa8ebf9b2
lazycodex <Hope_Of_dfinal@hotmail.com>
2021-02-16 02:24:12 +0900

    add notes preview

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var currentMusic = ['A','A','A','A','A','A','A','A'] // default music
+var currentMusic; // default music
 var jsonMusic; // musics from JSON
 
+/*Load Data from Backend*/
 function GetMusicFromJSON(){
 	fetch("https://raw.githubusercontent.com/lazycodex/xyz/main/samples-record.json")
 		.then(response => response.json())
@@ -13,16 +14,20 @@ function GetMusicFromJSON(){
 			arr.forEach((data) => { // get single note from array
 				console.log(data);
 			});
-			
 			var doc =  document.getElementById("musicList");
 			var newMusic = document.createElement("li");
 			//newMusic.id = 'div_id';
 			newMusic.innerHTML = '<a class="dropdown-item" href="#">'+data.name+'</a>';
 			doc.appendChild(newMusic);
+			if(data.name == 'Default'){ // Choose default music
+				currentMusic = data.val;
+				document.getElementById("musicNotes").innerHTML = currentMusic.join(" ");
+			}
 		  });
         });
 }
 
+/*Title Wave Effect*/
 function AnimateText(){
 	// Wrap every letter in a span
 	var textWrapper = document.querySelector('.anim1 .letters');
@@ -45,12 +50,14 @@ function AnimateText(){
 }
 
 
+/*Start Event On Load*/
 window.addEventListener('load', (event) => {
 	console.log('page is fully loaded');
 	GetMusicFromJSON();
 	AnimateText();
 });
 
+/*Dropdown Click Event*/
 function AddCustomEventListener() {
 	(function ($) {
 		"use strict";
@@ -59,7 +66,7 @@ function AddCustomEventListener() {
 		  $(".dropdown-menu li a").click(function(){
 			$(".btn:first-child").text($(this).text());
 			 $(".btn:first-child").val($(this).text());
-			//document.getElementById("musicName").innerHTML = "Playing: " + $(this).text();
+			//document.getElementById("musicName").innerHTML = "Playing: " + $(this).text(); // set text without animation
 			for(var i=0; i<jsonMusic.length;i++) {
 				var obj = jsonMusic[i];
 				if(obj.name == $(this).text()){
@@ -67,7 +74,8 @@ function AddCustomEventListener() {
 					currentMusic = obj.val;
 				}
 			}
-			 TypingEffect($(this).text()); // Use above line to set text without animation instead
+			MusicNameTypingEffect($(this).text()); 
+			MusicNotesTypingEffect(currentMusic.join(" ")); 
 		  });
 		});
 		
@@ -80,28 +88,42 @@ function AddCustomEventListener() {
     
 setTimeout(function(){AddCustomEventListener()}, 500); // 0.5 second delay for fetching
 
+/*TypeWriterEffects*/
 
-var captionLength = 0;
+var captionLength1 = 0;
+var captionLength2 = 0;
 var musicName = '';
 
 $(document).ready(function() {
-    captionEl = $('#musicName');
-    /*$('#test-typing').click(function(){
-        TypingEffect("Test");
-    });*/
+    msEl1 = $('#musicName');
+	msEl2 = $('#musicNotes');
+    /*$('#test-typing').click(function(){ MusicNameTypingEffect("Test"); });*/
 });
 
-function TypingEffect(text) {
-    musicName = $('input#user-musicName').val();
-    type(text);
+function MusicNameTypingEffect(text) {
+    TypeName(text);
 }
 
-function type(text) {
-    captionEl.html(text.substr(0, captionLength++));
-    if(captionLength < text.length+1) {
-        setTimeout(function(){type(text)}, 50);
+function MusicNotesTypingEffect(text) {
+    TypeNotes(text);
+}
+
+function TypeName(text) {
+    msEl1.html(text.substr(0, captionLength1++));
+    if(captionLength1 < text.length+1) {
+        setTimeout(function(){TypeName(text)}, 50);
     } else {
-        captionLength = 0;
+        captionLength1 = 0;
+        text = '';
+    }
+}
+
+function TypeNotes(text) {
+    msEl2.html(text.substr(0, captionLength2++));
+    if(captionLength2 < text.length+1) {
+        setTimeout(function(){TypeNotes(text)}, 50);
+    } else {
+        captionLength2 = 0;
         text = '';
     }
 }
