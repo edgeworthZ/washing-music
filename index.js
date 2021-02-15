a9c4d1b65ab0560cbb95dceafda22cce35eec951
lazycodex <Hope_Of_dfinal@hotmail.com>
2021-02-15 21:52:55 +0900

    edit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', (event) => {
   console.log('page is fully loaded');
-  fetch("https://raw.githubusercontent.com/lazycodex/xyz/main/s_sample.json")
+  fetch("https://raw.githubusercontent.com/lazycodex/xyz/main/samples.json")
 		.then(response => response.json())
 		.then((datas) => {
 			
