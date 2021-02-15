d97ee9ce03227149fbe56bbfb5a07228e6649301
lazycodex <Hope_Of_dfinal@hotmail.com>
2021-02-16 02:03:54 +0900

    add typewriter and jsonMusic

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+var currentMusic = ['A','A','A','A','A','A','A','A'] // default music
+var jsonMusic; // musics from JSON
+
 function GetMusicFromJSON(){
-	fetch("https://raw.githubusercontent.com/lazycodex/xyz/main/record-samples.json")
+	fetch("https://raw.githubusercontent.com/lazycodex/xyz/main/samples-record.json")
 		.then(response => response.json())
 		.then((datas) => {
-			
+			jsonMusic = datas;
+			console.log(jsonMusic);
 			datas.forEach((data) => {
 			console.log(data.name);
 			var arr = data.val;
-			arr.forEach((data) => {
+			arr.forEach((data) => { // get single note from array
 				console.log(data);
 			});
 			
@@ -50,13 +54,20 @@ window.addEventListener('load', (event) => {
 function AddCustomEventListener() {
 	(function ($) {
 		"use strict";
-		// Change text when select an item from dropdown-menu
+		// Select a music from dropdown-menu
 		$(function(){
 		  $(".dropdown-menu li a").click(function(){
-			
 			$(".btn:first-child").text($(this).text());
 			 $(".btn:first-child").val($(this).text());
-			 document.getElementById("musicName").innerHTML = "Playing: " + $(this).text();
+			//document.getElementById("musicName").innerHTML = "Playing: " + $(this).text();
+			for(var i=0; i<jsonMusic.length;i++) {
+				var obj = jsonMusic[i];
+				if(obj.name == $(this).text()){
+					console.log("Choose Music: "+obj.name+" Notes: "+obj.val);
+					currentMusic = obj.val;
+				}
+			}
+			 TypingEffect($(this).text()); // Use above line to set text without animation instead
 		  });
 		});
 		
@@ -67,4 +78,30 @@ function AddCustomEventListener() {
 	
 }
     
-setTimeout(function(){AddCustomEventListener()}, 1000); // 1 second delay for fetching
+setTimeout(function(){AddCustomEventListener()}, 500); // 0.5 second delay for fetching
+
+
+var captionLength = 0;
+var musicName = '';
+
+$(document).ready(function() {
+    captionEl = $('#musicName');
+    /*$('#test-typing').click(function(){
+        TypingEffect("Test");
+    });*/
+});
+
+function TypingEffect(text) {
+    musicName = $('input#user-musicName').val();
+    type(text);
+}
+
+function type(text) {
+    captionEl.html(text.substr(0, captionLength++));
+    if(captionLength < text.length+1) {
+        setTimeout(function(){type(text)}, 50);
+    } else {
+        captionLength = 0;
+        text = '';
+    }
+}
